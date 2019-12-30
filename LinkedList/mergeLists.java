da946b8ad4efea15f7d8849e2f22c1f62498dc04
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2019-12-30 01:17:55 -0500

    Add files via upload

---
+++ LinkedList/mergeLists.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the mergeLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode dummy = new SinglyLinkedListNode(0);
+        dummy.data = 0;
+        dummy.next = null;
+        SinglyLinkedListNode pre = dummy;
+        SinglyLinkedListNode P = head1;
+        SinglyLinkedListNode Q = head2;
+      
+        while(P!=null && Q!=null){
+            if(P.data<=Q.data){
+               pre.next = P;
+               P = P.next;
+            }else{
+               pre.next = Q;
+               Q = Q.next;
+            }
+            pre = pre.next;
+        }
+        if(P!=null){
+           pre.next = P;
+        }
+        if(Q!=null){
+           pre.next = Q;
+        }
+        return dummy.next;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
