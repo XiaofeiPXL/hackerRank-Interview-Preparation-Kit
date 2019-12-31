9c727753d1770d28217f9f0f03928391cfdd89f4
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2019-12-31 15:59:15 -0500

    Add files via upload

---
+++ Tree/preOrder.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void preOrder(Node root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<Node> stack = new Stack<Node>();
+
+        while(root!=null || !stack.isEmpty()){
+            while(root!=null){
+                res.add(root.data);
+                stack.push(root);
+                root = root.left;
+            }
+    
+            if(!stack.isEmpty()){
+               root = stack.pop();
+               root = root.right;
+            }
+        }
+        for(int i=0;i<res.size();i++){
+             System.out.print(res.get(i)+" ");
+        }
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }	
+}
