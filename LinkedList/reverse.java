85d3604c85954e7a65ddc2af19816256c72e2b7c
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2019-12-31 14:34:25 -0500

    Add files via upload

--- LinkedList/reverse.java
+++ LinkedList/reverse.java
@@ -8,39 +8,42 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static class SinglyLinkedListNode {
+    static class DoublyLinkedListNode {
         public int data;
-        public SinglyLinkedListNode next;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
 
-        public SinglyLinkedListNode(int nodeData) {
+        public DoublyLinkedListNode(int nodeData) {
             this.data = nodeData;
             this.next = null;
+            this.prev = null;
         }
     }
 
-    static class SinglyLinkedList {
-        public SinglyLinkedListNode head;
-        public SinglyLinkedListNode tail;
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
 
-        public SinglyLinkedList() {
+        public DoublyLinkedList() {
             this.head = null;
             this.tail = null;
         }
 
         public void insertNode(int nodeData) {
-            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
 
             if (this.head == null) {
                 this.head = node;
             } else {
                 this.tail.next = node;
+                node.prev = this.tail;
             }
 
             this.tail = node;
         }
     }
 
-    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
         while (node != null) {
             bufferedWriter.write(String.valueOf(node.data));
 
@@ -57,23 +60,26 @@ public class Solution {
     /*
      * For your reference:
      *
-     * SinglyLinkedListNode {
+     * DoublyLinkedListNode {
      *     int data;
-     *     SinglyLinkedListNode next;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
      * }
      *
      */
-    static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
-         SinglyLinkedListNode pre = null;
-         SinglyLinkedListNode cur = null;
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
 
-         while(head!=null){
-            cur = head.next;
-            head.next = pre;
-            pre = head;
-            head = cur;
-         }
-         return pre;
+           DoublyLinkedListNode pre = null;
+           DoublyLinkedListNode cur = null;
+
+           while(head!=null){
+               cur = head.next;
+               head.next = pre;
+               pre = head;
+               head = cur;
+           }
+
+           return pre;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -81,11 +87,11 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int tests = scanner.nextInt();
+        int t = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int testsItr = 0; testsItr < tests; testsItr++) {
-            SinglyLinkedList llist = new SinglyLinkedList();
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
 
             int llistCount = scanner.nextInt();
             scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -97,9 +103,9 @@ public class Solution {
                 llist.insertNode(llistItem);
             }
 
-            SinglyLinkedListNode llist1 = reverse(llist.head);
+            DoublyLinkedListNode llist1 = reverse(llist.head);
 
-            printSinglyLinkedList(llist1, " ", bufferedWriter);
+            printDoublyLinkedList(llist1, " ", bufferedWriter);
             bufferedWriter.newLine();
         }
 
