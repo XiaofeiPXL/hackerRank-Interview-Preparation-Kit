211883d0237427318ca6b1e334b99ef6571786a8
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2020-01-05 01:39:51 -0500

    Add files via upload

---
+++ Stack/editor.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        editor();
+    }
+
+    static void editor() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        Stack<String> operations = new Stack<String>();
+        operations.push(str);
+        int Q = Integer.parseInt(br.readLine());
+        String[] content;
+        for (int i = 0; i < Q; i++) {
+            content = br.readLine().split(" ");
+            switch (content[0]) {
+            case "1":
+                String W = content[1];
+                str = str + W;
+                operations.push(str);
+                break;
+            case "2":
+                int m = Integer.parseInt(content[1]);
+                str = str.substring(0, str.length() - m);
+                operations.push(str);
+                break;
+            case "3":
+                int n = Integer.parseInt(content[1]);
+                System.out.println(str.charAt(n - 1));
+                break;
+            case "4":
+                operations.pop();
+                str = operations.peek();
+            default:
+                break;
+            }
+        }
+    }
+}
+
