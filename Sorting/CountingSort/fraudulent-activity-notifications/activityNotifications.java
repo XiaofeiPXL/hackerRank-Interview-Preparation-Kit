42f77cb0f4a4906e58d698a804215bc39028f77f
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2020-01-25 02:03:15 -0500

    Add files via upload

---
+++ Sorting/CountingSort/fraudulent-activity-notifications/activityNotifications.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the activityNotifications function below.
+    public static int activityNotifications(int[] expenditure, int d){
+        int[] count = new int[201];
+        int result = 0;
+        for(int i = 0; i < d; i++){
+            count[expenditure[i]]++;
+        }
+        for(int i = d; i < expenditure.length; i++){
+            int median = getMedian(count, d);
+            if(median <= expenditure[i]){
+                result++;
+            }
+            count[expenditure[i-d]]--;
+            count[expenditure[i]]++;
+        }
+        return result;
+    }
+    public static int getMedian(int[] count, int d){
+        int sum = 0;
+        for(int i = 0; i < count.length; i++){
+            sum += count[i];
+            if((2*sum) == d){
+                return (2*i+1);
+            }else if((2*sum) > d){
+                return (i*2);
+            }
+        }
+        return 1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] expenditure = new int[n];
+
+        String[] expenditureItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int expenditureItem = Integer.parseInt(expenditureItems[i]);
+            expenditure[i] = expenditureItem;
+        }
+
+        int result = activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
