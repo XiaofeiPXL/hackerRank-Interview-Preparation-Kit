4b620f7c92bd8271f7c5e839b83cfd98cca1a3c7
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2020-02-02 01:23:30 -0500

    Add files via upload

---
+++ Greedy Algorithms/Max-Min/maxMin.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the maxMin function below.
+    static int maxMin(int k, int[] arr) {
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(arr);
+        int i = 0;
+        while (i < arr.length-k+1) {
+                int diff = arr[i + k - 1] - arr[i];
+                list.add(diff);
+                i++;
+        }
+        int res = Collections.min(list);
+        return res;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            arr[i] = arrItem;
+        }
+
+        int result = maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
