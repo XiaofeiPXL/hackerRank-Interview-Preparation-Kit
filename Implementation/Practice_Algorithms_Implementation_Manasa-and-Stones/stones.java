7f2b5ad0f4315f1dd82c2922a64f6b2c7d2101ad
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2020-02-18 23:14:21 -0500

    Add files via upload

---
+++ Implementation/Practice_Algorithms_Implementation_Manasa-and-Stones/stones.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the stones function below.
+    static int[] stones(int n, int a, int b) {
+         List<Integer> list = new LinkedList<Integer>();
+         for(int i=0;i<n;i++) {
+             if(!list.contains((n-i-1)*a+(i*b))) {
+                 list.add((n-i-1)*a+(i*b));
+             }
+         }
+        
+         int length = list.size();
+         int[] res = new int[length];
+         for(int j=0;j<length;j++) {
+             res[j] = list.get(j);
+         }
+         Arrays.sort(res);
+         return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int TItr = 0; TItr < T; TItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int a = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int b = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] result = stones(n, a, b);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
